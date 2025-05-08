abe878b1b914361f1b09d406425a6df10700538e
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-09 00:56:31 +0530

    Fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,20 @@
+import { Fragment } from "react/jsx-runtime";
+
 function ListGroup() {
-    return <ul className="list-group">
-    <li className="list-group-item active" aria-current="true">An active item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>
+  return (
+    <Fragment>
+        <h1>List Group</h1>
+        <ul className="list-group">
+          <li className="list-group-item active" aria-current="true">
+            An active item
+          </li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li>
+        </ul>
+    </Fragment>
+  );
 }
 
 export default ListGroup;
