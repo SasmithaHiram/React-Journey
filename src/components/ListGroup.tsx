913fc6418d335c18de42f0172c804f2d9c44be64
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-09 01:37:10 +0530

    Use Funtion - getMessage()

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,13 +2,17 @@ function ListGroup() {
   let items = ["Sri Lanka", "India", "New York", "Canada"];
   items = [];
 
+  const getMessage = () => {
+    return items.length === 0 ? <p>Not Found</p> : null;
+  };
+
   return (
     <>
       <ul className="list-group">
         <li className="list-group-item active" aria-current="true">
           Countries
         </li>
-        {items.length === 0 ? <p>Not Found</p> : null}
+        {getMessage()}
         {items.map((item) => (
           <li className="list-group-item" key={item}>
             {item}
