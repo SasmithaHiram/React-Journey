fc84ca9839b1947d05248716c2fbb1d891c0b476
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-09 01:12:40 +0530

    Dynamically Load Data

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,17 @@
 function ListGroup() {
+  const items = ["Sri Lanka", "India", "New York", "Canada"];
+
   return (
     <>
-        <h1>List Group</h1>
-        <ul className="list-group">
-          <li className="list-group-item active" aria-current="true">
-            An active item
-          </li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        <li className="list-group-item active" aria-current="true">
+          Countries
+        </li>
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
