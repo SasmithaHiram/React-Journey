b59b9e98c03a56154a57f7e2e848e8c42ce35289
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-09 13:55:33 +0530

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Sri Lanka", "India", "New York", "Canada"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Countries"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
