6dc7fc489e7a9475927abd3656ff860b5b789729
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-12 18:43:35 +0530

    Handling From Submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import Expandable from "./components/Expandable";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <Expandable>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio,
-        sequi vero debitis ipsum dolorum culpa quia eum pariatur aliquam
-        suscipit, placeat enim dolor velit veniam veritatis deserunt ea quam
-        natus nesciunt sint dolorem provident! Architecto, soluta. Mollitia
-        rerum tempore, saepe consequatur labore, eos expedita quibusdam ducimus
-        fuga pariatur numquam ullam provident, nam laborum? Eos natus fugiat cum
-        aliquam? Nisi, architecto magnam. Tempore maxime repellendus officia
-        iste, sit voluptatum ipsa, distinctio facilis ducimus molestias culpa
-        tenetur cum repellat doloribus mollitia dolore velit possimus sunt.
-        Ipsam officia inventore iusto in, praesentium magnam placeat alias,
-        natus nobis dicta magni quis, possimus blanditiis quaerat.
-      </Expandable>
+      <Form />
     </div>
   );
 }
