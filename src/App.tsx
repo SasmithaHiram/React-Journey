61177f9d8c4043ebd8a67dda30cd12ae859490b7
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-20 00:55:50 +0530

    Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { useEffect } from "react";
-
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+interface User {
+  id: number;
+  name: string;
+}
 const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    connect();
-    return () => disconnect();
-  });
-  return <div></div>;
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
