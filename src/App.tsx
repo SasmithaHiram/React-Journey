c8740a64f1d2f5a841151fb6cd9c909665ca6cb6
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-21 15:56:50 +0530

    Showing a Loading Indicator

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,25 @@ interface User {
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
     const fetchUsers = async () => {
+      setLoading(true);
       try {
         const res = await axios
           .get<User[]>("https://jsonplaceholder.typicode.com/users", {
             signal: controller.signal,
           })
-          .then((res) => setUsers(res.data));
+          .then((res) => {
+            setUsers(res.data);
+            setLoading(true);
+          });
       } catch (err) {
         if (err instanceof CanceledError) return;
         setError((err as AxiosError).message);
+        setLoading(false);
       }
     };
 
@@ -29,6 +35,7 @@ const App = () => {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
